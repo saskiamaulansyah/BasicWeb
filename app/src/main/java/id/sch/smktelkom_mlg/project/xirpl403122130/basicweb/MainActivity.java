a596a69e5dd2fa8094fe28b3ddafd24ecfb83183
ezaalfandy <Alyssavelia2980@gmail.com>
2016-11-19 13:43:18 +0700

    about us

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/MainActivity.java
@@ -1,8 +1,8 @@
 package id.sch.smktelkom_mlg.project.xirpl403122130.basicweb;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CS.class);
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.imageViewAB).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, us.class);
                 startActivity(intent);
             }
         });
